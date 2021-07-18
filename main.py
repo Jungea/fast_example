4b7bfaf290842f04d5f325afbb79fe635e547058
Jungea <jungea2241@gmail.com>
2021-07-18 12:18:34 +0900

    predict code 추가

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+"""
+uvicorn main:app --reload --host=0.0.0.0 --port=8000
+"""
+
 from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+from keras.models import load_model
+import numpy as np
+from PIL import Image
+
 app = FastAPI()
+mm = load_model('mnist_model.h5')
 
 
 class Item(BaseModel):
@@ -16,8 +25,18 @@ class Item(BaseModel):
 def index():
     return {"Hello": "World"}
 
-# url/items/2?q=content
 
+# url/predict
+@app.get("/predict")
+def predict():
+    img = Image.open("test2.png").convert("L")
+    img = np.resize(img, (1, 784))
+    img = ((np.array(img) / 255) - 1) * -1
+    np.argmax(mm.predict(img))
+    return {"result": "a"}
+
+
+# url/items/2?q=content
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
